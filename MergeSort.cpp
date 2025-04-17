36e9e750c9888d200328c2629013654f60782576
Nafa06 <naufalanggardi09@gmail.com>
2025-04-17 10:33:15 +0700

    Membuat fungsi input

---
+++ MergeSort.cpp
@@ -0,0 +1,18 @@
+// COMMIT 1: Membuat variabel dan menyiapkan array
+#include <iostream>
+using namespace std;
+
+// create main array & temporary array
+int arr[20], B[20];
+// n is array input size
+int n;
+
+// COMMIT 2: Membuat fungsi input
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
+    }
+}
