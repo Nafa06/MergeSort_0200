7ac99eb4df393d1a4f014e919cc556d725cbf678
Nafa06 <naufalanggardi09@gmail.com>
2025-04-17 11:03:32 +0700

    Menggabungkan sisa elemen array yang belum terurut

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -84,20 +84,4 @@ void mergeSort(int low, int high)
         i++;
         k++;
     }
-
-    // COMMIT 9: Menyalin hasil penggabungan kembali ke array utama
-    for (int x = low; x <= high; x++) // salin array sementara ke array asli
-    {
-        arr[x] = B[x];
-    }
-    // COMMIT 10: Membuat fungsi untuk menampilkan hasil pengurutan dan main function
-    void output();
-    {
-        cout << "\nData setelah diurutkan (Merge Sort): ";
-        for (int i = 0; i < n; i++)
-        {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-    }
 }
